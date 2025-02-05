0fe8cc580cfdf2df96de0be844b3c817e20d1abf
imad faouzi <66480683+imadfaouzi@users.noreply.github.com>
2025-02-05 17:35:55 +0100

    Create curl_fallback.sh

---
+++ curl_fallback.sh
@@ -0,0 +1,9 @@
+curl -X 'POST' \
+  'http://localhost:8000/conversation/get_responseLLM' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "sender": "stnffjhfFFFFjfhfhfjgjfjfjfjfjFFFhfjjjjhGGfhriGFng",
+  "message": "merci",
+  "canal": "FACEBOOK"
+}'
